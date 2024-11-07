0a3d6ad6795e65450022293cd775e2d5ae81f124
Gauripatole11 <gauripatole9@gmail.com>
2024-11-06 18:47:50 -0800

    Route/Auth.js to create user login and registration

---
+++ localized-alerts-backend/routes/auth.js
@@ -0,0 +1,77 @@
+// routes/auth.js -> for registration and login of user
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const { body, validationResult } = require("express-validator");
+const User = require("../models/User");
+
+const router = express.Router();
+
+// Register route
+router.post(
+  "/register",
+  [
+    body("email").isEmail().withMessage("Enter a valid email"),
+    body("password")
+      .isLength({ min: 11 })
+      .withMessage("Password must be at least 6 characters long"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    const { email, password } = req.body;
+    try {
+      let user = await User.findOne({ email });
+      if (user) return res.status(400).json({ message: "User already exists" });
+
+      const hashedPassword = await bcrypt.hash(password, 10);
+      user = new User({ email, password: hashedPassword });
+      await user.save();
+
+      const token = jwt.sign({ userId: user._id }, process.env.JWT_SECRET, {
+        expiresIn: "1h",
+      });
+      res.json({ token });
+    } catch (error) {
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+// Login route
+router.post(
+  "/login",
+  [
+    body("email").isEmail().withMessage("Enter a valid email"),
+    body("password").exists().withMessage("Password is required"),
+  ],
+  async (req, res) => {
+    const errors = validationResult(req);
+    if (!errors.isEmpty())
+      return res.status(400).json({ errors: errors.array() });
+
+    const { email, password } = req.body;
+    try {
+      const user = await User.findOne({ email });
+      if (!user)
+        return res.status(400).json({ message: "Invalid credentials" });
+
+      const isMatch = await bcrypt.compare(password, user.password);
+      if (!isMatch)
+        return res.status(400).json({ message: "Invalid credentials" });
+
+      const token = jwt.sign(
+        { userId: user._id, role: user.role },
+        process.env.JWT_SECRET,
+        { expiresIn: "1h" }
+      );
+      res.json({ token });
+    } catch (error) {
+      res.status(500).send("Server error");
+    }
+  }
+);
+
+module.exports = router;
